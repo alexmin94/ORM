65e59a93529e1731b3e585fbc7056f0abde08d4c
Alex <aminyakov94@gmail.com>
2023-06-30 12:57:21 -0500

    aded category gett route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,7 +6,12 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+  Category.findAll({
+    include:[Product]
+  })
+  .then((categories)=> res.json(categories))
+  .catch((err)=>res.status(500).json(err))
+});p
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
