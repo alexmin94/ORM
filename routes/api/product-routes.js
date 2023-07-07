1e3183aaf93bb8806ad6672d53c3c1b0711c8965
Alex <aminyakov94@gmail.com>
2023-07-07 14:36:21 -0500

    added delete routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -53,7 +53,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -119,6 +119,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id:req.params.id
+    },
+  })
+  .then((products)=>res.json(products))
+  .catch((err)=>res.status(500).json(err))
 });
 
 module.exports = router;
